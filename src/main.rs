d1d04bcfeaaf202d8d851fb5e5c69d81640e4fad
Artur Peixoto <arturokfya@gmail.com>
2026-06-17 01:19:53 -0300

    some speed-up

--- src/main.rs
+++ src/main.rs
@@ -212,7 +212,7 @@ pub async fn move_data(
 
             to.write_all(data_buf.read()).await?;
 
-            let Ok(_) = waste_sender.send(data_buf).await else {break};
+            let Ok(_) = waste_sender.send(data_buf).await else {continue};
         }
         to.flush().await?;
 
